b9670d98a00337c7d8baad634124c89530e28f04
May Natan <maynatan@Mays-MacBook-Pro.local>
2021-12-30 15:48:53 +0200

    change

--- routes/Login.js
+++ routes/Login.js
@@ -9,29 +9,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    User.findOne({Email:req.body.inputEmail,Password:req.body.inputPassword})
-        .exec()
-        .then(doc => {
-            User.findOneAndUpdate({_id:doc._id},{
-                Connected:true
-            }).exec();
-            console.log(doc);
-            if (doc)
-                res.redirect('/profile/'+doc._id);
-            else
-                res.status(500).json({
-                    message:"Username or Password is incorrect",
-                    user:req.body.inputUsername,
-                    password:req.body.inputPassword
-                });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                message:"Username or Password is incorrect",
-                user:req.body.inputUsername,
-                password:req.body.inputPassword
+        User.findOne({Email: req.body.inputEmail})
+            .exec()
+            .then(doc => {
+                if (doc) {
+                    console.log(doc.Password);
+                    console.log(GetPassword(doc.Salt, req.body.inputPassword));
+                    if (doc.Password == GetPassword(doc.Salt, req.body.inputPassword)) {
+                        User.findOneAndUpdate({_id: doc._id}, {
+                            Connected: true
+                        }).exec();
+                        res.redirect('/profile/' + doc._id);
+                    } else {
+                        window.confirm("Password is incorrect");
+                    }
+                } else
+                    res.status(500).json({
+                        message: "Email is incorrect"
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+                window.confirm("Username or Password is incorrect");
             });
-        });
+
 });
 module.exports = router;
