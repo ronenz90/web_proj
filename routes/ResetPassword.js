f44193fd8ea0d2c01bc15a8e68e237ddebfa2498
noy5277 <55356033+noy5277@users.noreply.github.com>
2022-01-03 01:22:09 +0200

    Reset password page

--- routes/ResetPassword.js
+++ routes/ResetPassword.js
@@ -2,6 +2,7 @@ var express = require('express');
 const mongoose = require('mongoose');
 const User = require("../models/user");
 const validator = require("validator");
+var passwordValidator=require('password-validator');
 const router = express.Router();
 
 
@@ -9,7 +10,33 @@ router.get('/:Email', function(req, res, next) {
     res.render('ResetPassword');
 });
 
-
+router.post('/:Email',function (req,res,next)
+{
+    var schema= new passwordValidator();
+    const password=SetPassword(req.body.inputPassword);
+    if(req.body.inputNewPassword==req.body.inputConfirmPassword)
+    {
+        if(validator.isStrongPassword(req.body.inputNewPassword,{minLength: 10, minLowercase: 1,
+            minUppercase: 1, minNumbers: 1, minSymbols: 1
+        })&&(schema.is().not().oneOf(['Passw0rd', 'Aa12345678!','P@ssw0rd123'])))
+        {
+            User.findOneAndUpdate({Email:req.params.Email},{
+                Password:password[1],
+                salt:password[0],
+                Tries:3
+            }).exec();
+            res.redirect('/');
+        }
+        else
+        {
+            res.status(500).send('Week Password');
+        }
+    }
+    else
+    {
+        res.status(500).send("passwords is not the same");
+    }
+});
 
 
 
